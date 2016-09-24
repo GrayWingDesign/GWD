41980990711794d2972cb09a64d070eb371b815e
grisl1 <graywingdesign@gmail.com>
2016-09-24 13:11:15 -0500

    remove dotten lines around anchors, added header file to child folder

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+<!-- allow remove dotted lines on anchors IE9 --><meta http-equiv="X-UA-Compatible" content="IE=9" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
